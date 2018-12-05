fefd0928a26c7a3e271aaa469b80c57cc110f87c
JoTiger <haoliang.jiang@outlook.com>
2018-12-05 23:21:45 +1100

    antialiasing

--- src/camera.rs
+++ src/camera.rs
@@ -1,8 +1,29 @@
+use ray::Ray;
 use vec3::Vec3f;
-struct Camera 
-{
-  origin : Vec3f,
+
+pub struct Camera {
+  origin: Vec3f,
   lower_left_corner: Vec3f,
-  horizontal : Vec3f,
-  vertical : Vec3f
+  horizontal: Vec3f,
+  vertical: Vec3f,
+}
+
+impl Default for Camera {
+  fn default() -> Camera {
+    Camera {
+      origin: Default::default(),
+      lower_left_corner: Vec3f::new(-2.0, -1.5, -1.0),
+      horizontal: Vec3f::new(4.0, 0.0, 0.0),
+      vertical: Vec3f::new(0.0, 3.0, 0.0),
+    }
+  }
+}
+
+impl Camera {
+  pub fn get_ray(&self, u: f32, v: f32) -> Ray {
+    Ray {
+      a: self.origin,
+      b: self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
+    }
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,12 @@ use hit::Hitable;
 use hit::HitableList;
 use sphere::Sphere;
 
+pub mod camera;
+use camera::Camera;
+
+extern crate rand;
+use rand::{thread_rng, Rng};
+
 use std::f32;
 
 fn color(ray: &Ray, world: &HitableList) -> Vec3f {
@@ -22,13 +28,7 @@ fn color(ray: &Ray, world: &HitableList) -> Vec3f {
       normal: Default::default()
     };
     if world.hit(ray, 0.0, f32::MAX, &mut rec) {
-        return 0.5 * Vec3f {
-            e: [
-                rec.normal.x() + 1.0,
-                rec.normal.y() + 1.0,
-                rec.normal.z() + 1.0,
-            ],
-        };
+        return 0.5 * Vec3f::new(rec.normal.x() + 1.0, rec.normal.y() + 1.0, rec.normal.z() + 1.0);
     } else {
         let unit_direction = unit_vector(ray.direction());
         let t = 0.5 * (unit_direction.y() + 1.0);
@@ -39,29 +39,32 @@ fn color(ray: &Ray, world: &HitableList) -> Vec3f {
 fn main() {
     let nx: i32 = 400;
     let ny: i32 = 300;
+    let ns: i32 = 100;
     print!("P3\n{} {}\n255\n", nx, ny);
-    let lower_left_corner = Vec3f {
-        e: [-2.0, -1.5, -1.0],
-    };
-    let horiztonal = Vec3f::new(4.0, 0.0, 0.0);
-    let vertical = Vec3f::new(0.0, 3.0, 0.0);
-    let origin : Vec3f = Default::default();
+    
 
     let mut world = HitableList { list: Vec::new() };
 
     world.list.push(Box::new(Sphere::new(&Vec3f::new(0.0, 0.0, -1.0), 0.5)));
-
     world.list.push(Box::new(Sphere::from((0.0, -100.5, -1.0, 100.0))));
+
+    let cam : Camera = Default::default();
+
+    let mut rng = thread_rng();
 
     for j in (0..ny).rev() {
         for i in (0..nx) {
-            let u = i as f32 / nx as f32;
-            let v = j as f32 / ny as f32;
-            let r = Ray {
-                a: origin,
-                b: lower_left_corner + u * horiztonal + v * vertical,
-            };
-            let col = color(&r, &world);
+            let mut col = Vec3f::default();
+            for s in (0..ns) {
+                let randu : f32 = rng.gen_range(0.0, 1.0);
+                let randv : f32 = rng.gen_range(0.0, 1.0);
+                let u = (i as f32 + randu) / nx as f32;
+                let v = (j as f32 + randv) / ny as f32;
+                let r = cam.get_ray(u, v);
+                let p = r.point_at_parameter(2.0);
+                col = col + color(&r, &world);
+            }
+            col = col / (ns as f32);
             let ir: i32 = (255.99 * col.r()) as i32;
             let ig: i32 = (255.99 * col.g()) as i32;
             let ib: i32 = (255.99 * col.b()) as i32;
